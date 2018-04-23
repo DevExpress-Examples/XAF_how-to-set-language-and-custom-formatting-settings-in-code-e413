12ad2434b58b52a0cb6471b032f990cd4e1b247b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Localization.Module.Win/WinModule.vb
@@ -0,0 +1,17 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace Localization.Module.Win
+	<ToolboxItemFilter("Xaf.Platform.Win")> _
+	Public NotInheritable Partial Class LocalizationWindowsFormsModule
+		Inherits ModuleBase
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
